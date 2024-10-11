7f41d836115e4d2471df69ff8e5aff0b5fefe461
macbook <macbook@Ruslan-mac.local>
2024-10-11 19:09:17 +0400

    added home tasks

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// namespace home_task1
+// {
+//     internal class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             for(int i = 1; i <=100; i++)
+//             {
+//                 if (i % 3 == 0 && i % 5 == 0)
+//                 {
+//                     System.Console.WriteLine(i);
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// namespace home_task2
+// {
+//     internal class Program 
+//     {
+//         static void Main(string[] args)
+//         {
+//             var ortalama = 96;
+//             if(ortalama < 65)
+//             {
+//                 Console.WriteLine("Kesildin");
+//             }
+//             else if (ortalama >= 65 && ortalama < 85)
+//             {
+//                 Console.WriteLine("Adi diplom");
+//             }
+//             else if (ortalama >= 85 && ortalama < 95)
+//             {
+//                 Console.WriteLine("Seref diplomu");
+//             }
+//             else if (ortalama >= 95)
+//             {
+//                 Console.WriteLine("Yuksek seref diplomu");
+//             }
+            
+            
+//         }
+//     }
+// }
